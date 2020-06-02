70b2c36a9434580f3a0134ed80a2db523009238b
Charles <charlesmaisonb@yahoo.Fr>
2020-06-02 23:18:12 +0200

    MaJ 02/06/20 - 23:15

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'bridgeqbdd.mysql.db');
+   define('DB_USERNAME', 'bridgeqbdd');
+   define('DB_PASSWORD', 'MyBdDMdM40');
+   define('DB_DATABASE', 'bridgeqbdd');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
